96166a5bfe0af431c20b62494d0fdb5a51b75c17
Yousef Abdelaty <yossifabdelaty@gmail.com>
2025-11-08 16:01:34 +0200

    Completed nav

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
@@ -7,6 +7,36 @@ import { Component } from '@angular/core';
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
-export class NavbarComponent {
+export class NavbarComponent  {
 
-}
+    linkList! :HTMLElement;
+    homeLink! :HTMLElement;
+    contactLink! :HTMLElement;
+    aboutLink! :HTMLElement;
+    signLink! :HTMLElement;
+
+     ngAfterViewInit(): void {
+
+      this.homeLink = document.getElementById('link1') as HTMLElement;
+      this.contactLink = document.getElementById('link2') as HTMLElement;
+      this.aboutLink = document.getElementById('link3') as HTMLElement;
+      this.signLink = document.getElementById('link4') as HTMLElement;
+      this.linkList = document.getElementById('linkList') as HTMLElement;
+
+        this.linkList.addEventListener('click' ,(event : any) =>{
+
+          if(event.target.tagName ==='LI'){
+
+            Array.from(this.linkList.children).forEach((el:any)=>{
+              el.classList.remove('active');
+              el.classList.add('inactive');
+
+            });
+
+            event.target.classList.add('active');
+            event.target.classList.remove('inactive');
+          }
+        });
+    }
+   
+  }
