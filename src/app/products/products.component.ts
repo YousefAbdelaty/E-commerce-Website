3a6e02608085f2d62b7c3868995631fbcd222611
Yousef Abdelaty <yossifabdelaty@gmail.com>
2026-04-21 20:51:16 +0200

    Made some progress on the products page

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,12 +1,30 @@
 import { Component } from '@angular/core';
-
+import { ProductCardComponent } from '../product-card/product-card.component';
+import { CommonModule } from '@angular/common';
+import { ProductsSharingServiceService } from '../Services/products.sharing.service.service';
 @Component({
   selector: 'app-products',
   standalone: true,
-  imports: [],
+  imports: [ProductCardComponent, CommonModule],
   templateUrl: './products.component.html',
   styleUrl: './products.component.css'
 })
 export class ProductsComponent {
+
+  constructor(public prods:ProductsSharingServiceService) { }
+  products:any[]=[];
+  priceRange:string="";
+
+
+  ngOnInit(){
+    this.prods.getAllProducts().subscribe((prods)=>{
+      this.products=prods;
+    });
+    console.log(this.products);
+  }
+
+  priceRangeChange(selectedRange: string) {
+    this.priceRange = selectedRange;
+  }
 
 }
